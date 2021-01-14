afb411a3cce34dac6240c5b6b5ed5641b1cb9432
Aykut Ulış <aykutulis@hotmail.com>
2021-01-15 02:14:58 +0300

    Auth reducers & actions

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,21 +3,26 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
+import { userLoginReducer } from './reducers/userReducers';
 
-const reducers = combineReducers({
+const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
 
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null;
+
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 
 const middlewares = [thunk];
 
-const store = createStore(reducers, initialState, composeWithDevTools(applyMiddleware(...middlewares)));
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middlewares)));
 
 export default store;
