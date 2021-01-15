1d8c56eaba0133e2707110f076a71dfc054336a8
Aykut Ulış <aykutulis@hotmail.com>
2021-01-15 04:44:08 +0300

    Show user in navbar & logout

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL } from '../constants/userConstants';
+import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT } from '../constants/userConstants';
 
 export const login = (email, password) => async (dispatch) => {
   try {
@@ -27,4 +27,10 @@ export const login = (email, password) => async (dispatch) => {
       payload: error.response && error.response.data.message ? error.response.data.message : error.message,
     });
   }
+};
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('userInfo');
+
+  dispatch({ type: USER_LOGOUT });
 };
