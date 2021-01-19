148496efbe71910c5a315d34d537e335fe0ebbe2
Aykut Ulış <aykutulis@hotmail.com>
2021-01-19 06:43:26 +0300

    Get single order backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,9 @@
 import asyncHandler from 'express-async-handler';
 import Order from '../models/orderModel.js';
 
+// @desc    Create new order
+// @route   POST /api/orders
+// @access  Private
 const createOrder = asyncHandler(async (req, res, next) => {
   const { orderItems, shippingAddress, paymentMethod, itemsPrice, shippingPrice, taxPrice, totalPrice } = req.body;
 
@@ -22,4 +25,18 @@ const createOrder = asyncHandler(async (req, res, next) => {
   res.status(201).json(createdOrder);
 });
 
-export { createOrder };
+// @desc    Get order by id
+// @route   GET /api/orders/:id
+// @access  Private
+const getSingleOrder = asyncHandler(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate('user', 'name email');
+
+  if (!order) {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+
+  res.json(order);
+});
+
+export { createOrder, getSingleOrder };
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { getAccessToRoute } from '../middlewares/authMiddleware.js';
-import { createOrder } from '../controllers/orderController.js';
+import { createOrder, getSingleOrder } from '../controllers/orderController.js';
 
 const router = express.Router();
 
 router.post('/', [getAccessToRoute], createOrder);
+router.get('/:id', [getAccessToRoute], getSingleOrder);
 
 export default router;
