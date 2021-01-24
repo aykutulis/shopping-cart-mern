18dfdb42400c761c96324c85361d8627b846d1d1
Aykut Ulış <aykutulis@hotmail.com>
2021-01-25 02:11:00 +0300

    Create product & Update product endpoints, sample.jpg

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -38,4 +38,51 @@ const deleteProduct = asyncHandler(async (req, res, next) => {
   }
 });
 
-export { getAllProducts, getSingleProduct, deleteProduct };
+// @desc    Create a product
+// @route   POST /api/products
+// @access  Private/Admin
+const createProduct = asyncHandler(async (req, res, next) => {
+  const product = new Product({
+    name: 'Sample name',
+    price: 0,
+    user: req.user,
+    image: '/images/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description',
+  });
+
+  const createdProduct = await product.save();
+
+  res.status(201).json(createdProduct);
+});
+
+// @desc    Update a product
+// @route   PUT /api/products/:id
+// @access  Private/Admin
+const updateProduct = asyncHandler(async (req, res, next) => {
+  const { name, price, description, image, brand, category, countInStock } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+
+  product.name = name;
+  product.price = price;
+  product.description = description;
+  product.image = image;
+  product.brand = brand;
+  product.category = category;
+  product.countInStock = countInStock;
+
+  const updatedProduct = await product.save();
+
+  res.json(updatedProduct);
+});
+
+export { getAllProducts, getSingleProduct, deleteProduct, createProduct, updateProduct };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { getAccessToRoute, isAdmin } from '../middlewares/authMiddleware.js';
-import { deleteProduct, getAllProducts, getSingleProduct } from '../controllers/productController.js';
+import { createProduct, deleteProduct, getAllProducts, getSingleProduct, updateProduct } from '../controllers/productController.js';
 
 const router = express.Router();
 
 router.get('/', getAllProducts);
 router.get('/:id', getSingleProduct);
 router.delete('/:id', [getAccessToRoute, isAdmin], deleteProduct);
+router.post('/', [getAccessToRoute, isAdmin], createProduct);
+router.put('/:id', [getAccessToRoute, isAdmin], updateProduct);
 
 export default router;
